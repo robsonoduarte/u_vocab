1b994ab2a247d61c2f2d8ee78c80a7c156ae6f47
Robson Duarte <robosn.o.d@gmail.com>
2023-01-10 12:59:06 -0300

    :pray:

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-/*  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const UVocab());
-
-    expect(find.text('uVocab'), findsOneWidget);
-  });*/
-}
