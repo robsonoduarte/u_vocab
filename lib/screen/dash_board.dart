ad63e67507eade8881d4fd8159d1478b4e33ec0d
Robson Duarte <robson.o.d@gmail.com>
2023-02-03 19:23:26 -0300

    first version of shuffle word to google define (#9)
    Co-authored-by: Robson Duarte <robosn.o.d@gmail.com>

--- lib/screen/dash_board.dart
+++ lib/screen/dash_board.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:u_vocab/service/worlds.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
@@ -8,6 +9,7 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
+  final _worlds = Worlds();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,21 +19,46 @@ class _DashBoardState extends State<DashBoard> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
+          children: <Widget>[
             Text(
-              'uVocab',
+              'Total Worlds : ${_worlds.total}',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(80, 20, 80, 0),
+              child: ElevatedButton(
+                  onPressed: () async {
+                    launchUrl(
+                      Uri.parse(
+                          'https://www.google.com/search?q=define+${_worlds.shuffle()}'),
+                    );
+                  },
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.shuffle,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('shuffle one world'),
+                      ),
+                    ],
+                  )),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+/*      floatingActionButton: FloatingActionButton(
         onPressed: () {
           context.go("/words");
         },
         child: const Icon(
           Icons.add,
         ),
-      ),
+      ),*/
     );
   }
 }
